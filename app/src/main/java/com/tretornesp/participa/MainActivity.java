876f42a17b17d939989e21d536ee5850dc4a4f43
tretornesp <xabier.iglesias.perez@udc.es>
2023-01-01 23:46:03 +0100

    Maps fragment ready

--- app/src/main/java/com/tretornesp/participa/MainActivity.java
+++ app/src/main/java/com/tretornesp/participa/MainActivity.java
@@ -3,6 +3,7 @@ package com.tretornesp.participa;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -15,33 +16,33 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "mainActivity";
 
+    private final BottomNavigationView.OnItemSelectedListener itemSelectedListener = item -> {
+        Fragment selectedFragment = null;
+
+        if (item.getItemId() == R.id.list_item) {
+            Log.d(MainActivity.TAG, "Navigating to List");
+            selectedFragment = new ListFragment();
+        } else if (item.getItemId() == R.id.new_item) {
+            Log.d(MainActivity.TAG, "Navigating to New");
+        } else if (item.getItemId() == R.id.map_item) {
+            Log.d(MainActivity.TAG, "Navigating to Map");
+            selectedFragment = new MapsFragment();
+        } else if (item.getItemId() == R.id.profile_item) {
+            Log.d(MainActivity.TAG, "Navigating to Profile");
+        }
+
+        if (selectedFragment != null) getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        return true;
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(
-                new NavigationBarView.OnItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.list_item:
-                                Log.d(MainActivity.TAG, "Navigating to List");
-                                break;
-                            case R.id.new_item:
-                                Log.d(MainActivity.TAG, "Navigating to New");
-                                break;
-                            case R.id.map_item:
-                                Log.d(MainActivity.TAG, "Navigating to Map");
-                                break;
-                            case R.id.profile_item:
-                                Log.d(MainActivity.TAG, "Navigating to Profile");
-                                break;
-                        }
-                        return true;
-                    }
-                }
-        );
+        bottomNavigationView.setOnItemSelectedListener(itemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ListFragment()).commit();
     }
 }
